a2881f4272f64e7d8998ccaf89591e8dde96d8f5
Shaun Newsum <shaun.newsum@gmail.com>
2018-06-02 17:45:41 -0700

    increased timeout to 60 mins

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ var sendRequest = async function(ctx, url, data){
 		// Sign JSON
 		let signedData = signRequest(ctx, json);
 		let config = {
-			timeout: 5000,
+			timeout: 60000,
 			headers: {
 				'X-ICObench-Key': ctx.publicKey,
 				'X-ICObench-Sig': signedData,
